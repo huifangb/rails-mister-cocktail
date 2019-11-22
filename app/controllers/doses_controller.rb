86275a8be7125e1923364c4a8820410f43cf6aa6
huifang boo <huifangb@gmail.com>
2019-11-22 14:37:07 +0800

    added images

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -5,18 +5,20 @@ class DosesController < ApplicationController
   end
 
   def create
-    @cocktail = Cocktail.find(params[:cocktail_id])
     @dose = Dose.new(dose_params)
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    @dose.cocktail = @cocktail
     if @dose.save
       redirect_to cocktail_path(@cocktail)
     else
       render :new
+      raise
     end
   end
 
 
   def dose_params
-    params.require(:dose).permit(:description)
+    params.require(:dose).permit(:ingredient_id, :description)
   end
 
 end
