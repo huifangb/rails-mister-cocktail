11272731c0357dfd5f77731d7f2ec7d467a12b8a
huifang boo <huifangb@gmail.com>
2019-11-22 13:43:27 +0800

    added images

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,7 +7,10 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 # require 'json'
 # require 'open-uri'
+Cocktail.destroy_all
+Dose.destroy_all
 Ingredient.destroy_all
+
 
 # url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
 # ingredient_serialized = open(url).read
@@ -22,9 +25,9 @@ Ingredient.destroy_all
 # Ingredient.create(name: "ice")
 # Ingredient.create(name: "mint leaves")
 
-c1= Cocktail.create(name: "Singapore Sling", image: "https://res.cloudinary.com/fangb/image/upload/v1574391352/cocktail/photo-1551734322-f43b0798585a_q1suf4.jpg")
-c2= Cocktail.create(name: "Negroni", image: "https://res.cloudinary.com/fangb/image/upload/v1574393407/cocktail/olena-sergienko-JjGLEN7T8xI-unsplash_lb1d9k.jpg")
-c3=Cocktail.create(name: "Gin Martini", image: "https://res.cloudinary.com/fangb/image/upload/v1574393404/cocktail/photo-1544145945-f90425340c7e_b7mnmy.jpg")
+c1= Cocktail.create(name: "Singapore Sling", remote_image_url: "https://res.cloudinary.com/fangb/image/upload/v1574391352/cocktail/photo-1551734322-f43b0798585a_q1suf4.jpg")
+c2= Cocktail.create(name: "Negroni", remote_image_url: "https://res.cloudinary.com/fangb/image/upload/v1574393407/cocktail/olena-sergienko-JjGLEN7T8xI-unsplash_lb1d9k.jpg")
+c3=Cocktail.create(name: "Gin Martini", remote_image_url: "https://res.cloudinary.com/fangb/image/upload/v1574393404/cocktail/photo-1544145945-f90425340c7e_b7mnmy.jpg")
 
 i1= Ingredient.create(name: "Lemon")
 i2= Ingredient.create(name: "Campari")
